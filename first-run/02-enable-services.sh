2dfbf1333af48dad11793e5eb2e731c0c7cb53dd
PhotonQuantum <cy.n01@outlook.com>
2019-08-26 18:39:52 +0800

    update default services

--- first-run/02-enable-services.sh
+++ first-run/02-enable-services.sh
@@ -7,6 +7,10 @@ sudo systemctl enable --now systemd-networkd-wait-online
 sudo systemctl enable --now boinc-client
 sudo systemctl enable --now iptables
 sudo systemctl enable --now ufw
-sudo systemctl enable intel-undervolt
+sudo systemctl enable --now intel-undervolt
 sudo systemctl enable lightdm
 systemctl --user enable --now onedrive
+sudo systemctl enable --now avahi-daemon
+sudo systemctl enable gopreload
+sudo systemctl enable mk-lvm-snapshots
+sudo systemctl enable tlp tlp-sleep
